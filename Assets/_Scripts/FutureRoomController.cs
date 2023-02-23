a5be5bd2b4412395d9511313edd8aacc005e8ceb
aeric90 <ericdemarbre@cmail.carleton.ca>
2023-02-23 11:41:17 -0500

    Network test completed. Added USB Key and Vase

--- Assets/_Scripts/FutureRoomController.cs
+++ Assets/_Scripts/FutureRoomController.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class FutureRoomController : MonoBehaviour
+public class FutureRoomController : MonoBehaviourPunCallbacks
 {
     MailboxController mailbox = null;  // Holds the current object's mailbox object
     public GameObject teleport_area_2;
 
     private void Awake()
     {
-        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.AutomaticallySyncScene = false;
     }
 
     // Start is called before the first frame update
@@ -56,12 +57,44 @@ public class FutureRoomController : MonoBehaviour
     {
         if(PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.JoinRandomRoom();
+            //PhotonNetwork.JoinRandomRoom();
+            PhotonNetwork.JoinRoom("TE_TEST");
         }
         else
         {
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = "1";
         }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        //PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRoom("TE_TEST");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom("TE_TEST", new RoomOptions());
+    }
+        
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        Debug.Log(message);
     }
 }
