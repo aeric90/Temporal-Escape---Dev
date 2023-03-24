cbb42c7ef0bf64df18b93db466a8767a8082efd8
ewaldram <60678943+ewaldram@users.noreply.github.com>
2023-03-24 19:45:11 -0400

    Update InGameUIController.cs
    Accessibility and Exit Controls done

--- Assets/_Scripts/InGameUIController.cs
+++ Assets/_Scripts/InGameUIController.cs
@@ -11,6 +11,10 @@ public class InGameUIController : MonoBehaviour
     public GameObject leftRayController;
     public GameObject rightRayController;
 
+    public GameObject accessibilityControls;
+    public GameObject soundControls;
+    public GameObject exitControls;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +38,48 @@ public class InGameUIController : MonoBehaviour
     }
 
     public void RayGrabButton()
+    {
+        leftRayController.SetActive(!leftRayController.activeSelf);
+        rightRayController.SetActive(!rightRayController.activeSelf);
+    }
+
+    public void AccessibilityMenuButton()
+    {
+        accessibilityControls.SetActive(true);
+        soundControls.SetActive(false);
+        exitControls.SetActive(false);
+    }
+
+    public void SoundMenuButton()
+    {
+        accessibilityControls.SetActive(false);
+        soundControls.SetActive(true);
+        exitControls.SetActive(false);
+    }
+
+    public void ExitMenuButton()
+    {
+        accessibilityControls.SetActive(false);
+        soundControls.SetActive(false);
+        exitControls.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void TeleportToggle()
+    {
+       teleportationProvider.enabled = !teleportationProvider.enabled;
+    }
+
+    public void RotateToggle()
+    {
+        snapTurnProvider.enabled = !snapTurnProvider.enabled;
+    }
+
+    public void RayGrabToggle()
     {
         leftRayController.SetActive(!leftRayController.activeSelf);
         rightRayController.SetActive(!rightRayController.activeSelf);
