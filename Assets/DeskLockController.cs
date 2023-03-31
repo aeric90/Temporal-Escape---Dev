caaecf8ff4add0ad3ac2e738a8af6b371ef9adb8
aeric90 <ericdemarbre@cmail.carleton.ca>
2023-03-31 06:08:18 -0400

    Updates desk and book puzzles

--- Assets/DeskLockController.cs
+++ Assets/DeskLockController.cs
@@ -13,9 +13,12 @@ public class DeskLockController : MonoBehaviour
     public List<GameObject> deskLockButtons = new List<GameObject>();
     public List<Material> deskLockButtonOffMats = new List<Material>();
     public List<Material> deskLockButtonOnMats = new List<Material>();
+    public List<Material> deskLockButtonWrongMats = new List<Material>();
 
     public GameObject lockOpenButton;
+    public Material lockOffmat;
     public Material lockOpenMat;
+    public Material lockWrongMat;
 
     private List<int> codeEntered = new List<int>();
     public List<int> unlockCode = new List<int>();
@@ -34,11 +37,7 @@ public class DeskLockController : MonoBehaviour
 
     public void pushLockButton(int i)
     {
-        if(codeEntered.Contains(i))
-        {
-            codeEntered.Remove(i);
-            deskLockButtons[i].GetComponent<MeshRenderer>().material = deskLockButtonOffMats[i];
-        } else
+        if(!codeEntered.Contains(i))
         {
             codeEntered.Add(i);
             deskLockButtons[i].GetComponent<MeshRenderer>().material = deskLockButtonOnMats[i];
@@ -55,6 +54,9 @@ public class DeskLockController : MonoBehaviour
             new_message.Close_Tags();
             new_message.Date_Time = DateTime.Now.ToString();
             mailbox.Send_To_Sequence(new_message);
+        } else
+        {
+            StartCoroutine(CodeWrong());
         }
     }
 
@@ -62,9 +64,15 @@ public class DeskLockController : MonoBehaviour
     {
         bool checkLock = true;
 
-        foreach (int i in unlockCode)
+        if (codeEntered.Count == 4)
         {
-            if (!codeEntered.Contains(i)) checkLock = false;
+            for (int i = 0; i < unlockCode.Count; i++)
+            {
+                if (unlockCode[i] != codeEntered[i]) checkLock = false;
+            }
+        } else
+        {
+            checkLock = false;
         }
 
         return checkLock;
@@ -72,12 +80,12 @@ public class DeskLockController : MonoBehaviour
 
     private void EnableLock()
     {
-        lock_canvas.gameObject.SetActive(true);
+        lock_canvas.enabled = true;
     }
 
     private void DisableLock()
     {
-        lock_canvas.gameObject.SetActive(false);
+        lock_canvas.enabled= false;
     }
 
     private void CheckMailbox()
@@ -102,5 +110,50 @@ public class DeskLockController : MonoBehaviour
             mailbox.Remove_Message(message); // Remove the processed message
             message = mailbox.Get_Message(); // Get the next message, if any
         }
+    }
+
+    private void ResetLock()
+    {
+        for (int buttonID = 0; buttonID < deskLockButtons.Count; buttonID++)
+        {
+            deskLockButtons[buttonID].GetComponent<MeshRenderer>().material = deskLockButtonOffMats[buttonID];
+        }
+        lockOpenButton.GetComponent<MeshRenderer>().material = lockOffmat;
+        EnableLock();
+        codeEntered.Clear();
+    }
+
+    IEnumerator CodeWrong()
+    {
+        DisableLock();
+
+        bool flash = true;
+
+        for (int flashCount = 0; flashCount <= 5; flashCount++) {
+            for(int buttonID = 0; buttonID < deskLockButtons.Count; buttonID++)
+            {
+                if(flash)
+                {
+                    deskLockButtons[buttonID].GetComponent<MeshRenderer>().material = deskLockButtonWrongMats[buttonID];
+                } else
+                {
+                    deskLockButtons[buttonID].GetComponent<MeshRenderer>().material = deskLockButtonOffMats[buttonID];
+                }
+            }
+
+            if (flash)
+            {
+                lockOpenButton.GetComponent<MeshRenderer>().material = lockWrongMat;
+            }
+            else
+            {
+                lockOpenButton.GetComponent<MeshRenderer>().material = lockOffmat;
+            }
+
+            yield return new WaitForSeconds(0.2f);
+            flash = !flash;
+        }
+
+        ResetLock();
     }
 }
